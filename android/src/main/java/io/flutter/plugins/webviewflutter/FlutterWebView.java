408c3b87a5cd01b1d9649cb2d9a3569b87c86162
zhaoqi <550655294@qq.com>
2021-01-08 09:59:08 +0800

    webview——文字大小

--- android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -94,7 +94,9 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     // Allow local storage.
     webView.getSettings().setDomStorageEnabled(true);
     webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-
+    webView.getSettings().setUseWideViewPort(true);
+    webView.getSettings().setLoadWithOverviewMode(true);
+    webView.getSettings().setTextZoom(100);
     // Multi windows is set with FlutterWebChromeClient by default to handle internal bug: b/159892679.
     webView.getSettings().setSupportMultipleWindows(true);
     webView.setWebChromeClient(new FlutterWebChromeClient());
